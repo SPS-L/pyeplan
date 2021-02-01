766e4a0ee5ff11f3e353ae4b91865d0bd179d1e4
apetros <apetros@pm.me>
2021-02-01 15:46:57 +0200

    Updated license and version

--- pyeplan/__init__.py
+++ pyeplan/__init__.py
@@ -3,7 +3,7 @@
 """Python library for planning and operation of resilient microgrids."""
 
 __name__ = "pyeplan"
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = "Shahab Dehghan and Petros Aristidou"
 __copyright__ = ""
 __license__ = "GPL-3.0"

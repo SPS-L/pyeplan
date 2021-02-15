ee71df4678be23cb467022cff46c542cb783b47b
Petros Aristidou <apetros@users.noreply.github.com>
2021-02-15 14:00:25 +0200

    Update __init__.py

--- pyeplan/__init__.py
+++ pyeplan/__init__.py
@@ -4,13 +4,13 @@
 
 __name__ = "pyeplan"
 __version__ = '0.3'
-__author__ = u'2021 Shahab Dehghan (ICL), Agnes Nakiganda (UoL), Petros Aristidou (CUT)'
+__author__ = u'2021 Shahab Dehghan, Agnes Nakiganda, Petros Aristidou'
 __copyright__ = u'2021 Shahab Dehghan, Agnes Nakiganda, Petros Aristidou'
 __license__ ='Apache License 2.0',
 __maintainer__ = "s.dehghan@ieee.org"
 __email__ = "s.dehghan@ieee.org"
 __url__ = "https://pyeplan.readthedocs.io/"
-__status__ = "3 - Alpha"
+__status__ = "Development"
 
 
 from .operation import opersys

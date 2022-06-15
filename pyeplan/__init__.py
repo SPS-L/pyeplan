f680988acb8b6263d24a9145d6874907db943a07
apetros <apetros@pm.me>
2022-06-15 11:02:00 +0300

    Updated to 0.4.3

--- pyeplan/__init__.py
+++ pyeplan/__init__.py
@@ -4,8 +4,8 @@
 
 __name__ = "pyeplan"
 __version__ = '0.4.3'
-__author__ = u'2021 Shahab Dehghan, Agnes Nakiganda, Petros Aristidou'
-__copyright__ = u'2021 Shahab Dehghan, Agnes Nakiganda, Petros Aristidou'
+__author__ = u'2022 Shahab Dehghan, Agnes Nakiganda, Petros Aristidou'
+__copyright__ = u'2022 Shahab Dehghan, Agnes Nakiganda, Petros Aristidou'
 __license__ ='Apache License 2.0',
 __maintainer__ = "s.dehghan@ieee.org"
 __email__ = "s.dehghan@ieee.org"
